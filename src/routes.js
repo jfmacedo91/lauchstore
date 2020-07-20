fb97ed76f55628361a30f10c1a9ed8bd3eb6a62d
Jean Macedo <jfmacedo91@gmail.com>
2020-07-20 19:21:22 -0300

    added page create

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const routes = express.Router()
+const productController = require('./app/controllers/product')
 
 routes.get('/', (req, res) => {
   return res.render('layout.njk')
+})
+
+routes.get('/products/create', productController.create)
+
+routes.get('/ads/create', (req, res) => {
+  return res.render('/products/create')
 })
 
 module.exports = routes
