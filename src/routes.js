287cb587ac5bc8f8f22ec7366841ce26c724ba98
Jean Macedo <jfmacedo91@gmail.com>
2020-07-21 23:11:44 -0300

    added edit page and delete product

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,10 @@ routes.get('/', (req, res) => {
 })
 
 routes.get('/products/create', productController.create)
+routes.get('/products/:id/edit', productController.edit)
 routes.post('/products', productController.post)
+routes.put('/products', productController.put)
+routes.delete('/products', productController.delete)
 
 // Alias (Atalhos)
 routes.get('/ads/create', (req, res) => {
